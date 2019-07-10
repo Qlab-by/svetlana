f73df4a53ba1434f507bc4e9a1e7538a96f75b28
Qlab-by <52493260+Qlab-by@users.noreply.github.com>
2019-07-10 14:04:05 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
 		//right
 		if ( event.which == 39 ) {
 			console.log('right');
-		 if(right_pos - step > 10)
+		 if(right_pos - step > 0)
 		    $('.player').css('right', (right_pos - step) + 'px');
 		}
 		//top
